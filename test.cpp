0c7ce9fcc5e9099a05c0ceb8785ab1afc1260fca
BuildTools <unconfigured@null.spigotmc.org>
2016-01-14 15:29:21 +1000

    Added base files

---
+++ test.cpp
@@ -0,0 +1,17 @@
+//
+//  test.cpp
+//  
+//
+//  Created by WEZLEY VARTY on 14/01/2016.
+//
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main(void)
+{
+    cout << "hello world!" << endl;
+    return 0;
+}
